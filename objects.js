f79a50068060518490a6535f32032ef0da543d09
ji sun lee <ellijs@hotmail.com>
2021-03-23 05:42:55 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {name : "title"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  Object.assign({}, playlist, {artistName: ['songTitle']});
+  playlist[artistName] = songTitle;
   playlist;
 }
 
